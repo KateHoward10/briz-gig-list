e0ea264d2d57e4a592285b395c36dc697cbc76b0
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-14 08:57:29 +0100

    Update feed layout

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,6 @@
 class ProfileController < ApplicationController
   def index
     @actions = Action.where(user_id: current_user.id).order(updated_at: :desc).limit(20)
+    @actions_by_date = @actions.group_by {|a| a.updated_at.to_date }
   end
 end
--- app/helpers/gigs_helper.rb
+++ app/helpers/gigs_helper.rb
@@ -14,4 +14,12 @@ module GigsHelper
       datetime.localtime().strftime("%a %d %b %H:%M")
     end
   end
+
+  def format_time(datetime)
+    if datetime.blank?
+      nil
+    else
+      datetime.localtime().strftime("%H:%M")
+    end
+  end
 end
