d1f515e1dac28f1d69669ecadfcc71ff7a80ae80
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-14 11:56:39 +0100

    Format date timings

--- app/helpers/gigs_helper.rb
+++ app/helpers/gigs_helper.rb
@@ -22,4 +22,39 @@ module GigsHelper
       datetime.localtime().strftime("%H:%M")
     end
   end
+
+  def format_google_date(dt)
+    (dt.date || dt.date_time).strftime("%a %d %b")
+  end
+
+  def format_google_datetime(dt)
+    if dt.date_time.blank?
+      nil
+    else
+      (dt.date_time).strftime("%H:%M")
+    end
+  end
+
+  def format_google_dates(gig)
+    start_date = format_google_date(gig.start)
+    end_date = format_google_date(gig.end)
+    "#{start_date}#{" —  #{end_date}" if start_date != end_date}"
+  end
+
+  def format_google_times(gig)
+    start_date = format_google_date(gig.start)
+    end_date = format_google_date(gig.end)
+    diff_dates = start_date != end_date
+
+    start_time = format_google_datetime(gig.start)
+    end_time = format_google_datetime(gig.end)
+    diff_times = start_time != end_time
+
+    "
+      #{start_date}
+      #{", #{start_time}" if start_time.present?}
+      #{" —  #{end_date}" if diff_dates}
+      #{" —  #{end_time}" if diff_times}
+    "
+  end
 end
