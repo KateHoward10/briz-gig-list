1d086898e9209f9e6d31f6b6585c67419cc500e9
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-18 13:03:37 +0100

    Include replies in profile feed

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,9 @@ class ProfileController < ApplicationController
   def index
     @gigs = Gig.where(user_id: current_user.id).order(created_at: :desc).limit(3).to_a
     @responses = Response.where(user_id: current_user.id).order(created_at: :desc).limit(3).to_a
-    @posts = Post.where(user_id: current_user.id).order(created_at: :desc).limit(3).to_a
+    @posts = Post.joins(:parent).where(user_id: current_user.id)
+                 .or(Post.where(parent: { user_id: current_user.id }))
+                 .order(created_at: :desc).limit(3).to_a
     @actions = (@gigs + @responses + @posts).sort! { |a, b| b.created_at <=> a.created_at }
     @actions_by_date = @actions.group_by {|a| a.updated_at.to_date }
   end
