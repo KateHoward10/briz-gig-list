920655d51e73a3c61750970c0534c945cf0161b8
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-18 15:12:23 +0100

    Include links in Feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -5,7 +5,8 @@ class FeedController < ApplicationController
     @gigs = Gig.order(created_at: :desc).limit(5).to_a
     @responses = Response.order(created_at: :desc).limit(5).to_a
     @posts = Post.order(created_at: :desc).limit(5).to_a
-    @actions = (@gigs + @responses + @posts).sort! { |a, b| b.created_at <=> a.created_at }
+    @links = Link.order(created_at: :desc).limit(5).to_a
+    @actions = (@gigs + @responses + @posts + @links).sort! { |a, b| b.created_at <=> a.created_at }
     @actions_by_date = @actions.group_by {|a| a.created_at.to_date }
   end
 end
