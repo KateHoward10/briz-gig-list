8a57a00762c59918853f75bcbb5444b95558502c
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-19 16:21:06 +0100

    Scroll to bottom of posts

--- app/javascript/controllers/places_controller.js
+++ app/javascript/controllers/places_controller.js
@@ -12,16 +12,5 @@ export default class extends Controller {
   initMap() {
     this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
     this.autocomplete.setFields(["address_components", "geometry", "icon", "name"])
-    this.autocomplete.addListener("place_changed", this.placeChanged.bind(this))
-  }
-
-  placeChanged() {
-    let place = this.autocomplete.getPlace()
-
-    if (!place.geometry) {
-      window.alert(`No details available for input: ${place.name}`)
-      return
-    }
-
   }
 }
