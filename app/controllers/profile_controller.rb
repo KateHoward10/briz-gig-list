711308d7a0b40a055b1f13026bec3e3835326244
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-25 14:18:19 +0100

    Include links in profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,7 +5,8 @@ class ProfileController < ApplicationController
     @posts = Post.joins(:parent).where(user_id: current_user.id)
                  .or(Post.where(parent: { user_id: current_user.id }))
                  .order(created_at: :desc).to_a
-    @actions = (@gigs + @responses + @posts).sort! { |a, b| b.created_at <=> a.created_at }
+    @links = Link.order(created_at: :desc).to_a
+    @actions = (@gigs + @responses + @posts + @links).sort! { |a, b| b.created_at <=> a.created_at }
     @grouped_actions = @actions.group_by { |a| a.created_at.to_date }
     @actions_by_date = Kaminari.paginate_array(@grouped_actions, total_count: @grouped_actions.count).page(params[:page]).per(3)
   end
