ea616e9095d9b3dfd8ec7f44c5af8d1c40aa4965
Kate Howard <k.l.howard.1992@gmail.com>
2024-04-28 15:40:08 +0100

    List own responses on Profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,13 +1,9 @@
 class ProfileController < ApplicationController
+  before_action :authenticate
+
   def index
-    @gigs = Gig.where(user_id: current_user.id).order(created_at: :desc).to_a
-    @responses = Response.where(user_id: current_user.id).order(created_at: :desc).to_a
-    @posts = Post.joins(:parent).where(user_id: current_user.id)
-                 .or(Post.where(parent: { user_id: current_user.id }))
-                 .order(created_at: :desc).to_a
-    @links = Link.order(created_at: :desc).to_a
-    @actions = (@gigs + @responses + @posts + @links).sort! { |a, b| b.created_at <=> a.created_at }
-    @grouped_actions = @actions.group_by { |a| a.created_at.to_date }
-    @actions_by_date = Kaminari.paginate_array(@grouped_actions, total_count: @grouped_actions.count).page(params[:page]).per(3)
+    @responses = Response.joins(:gig).where(user_id: current_user.id).where(gig: Gig.where("start_date >= ?", Date.today))
+    @going = @responses.where(status: "Going").order("gigs.start_date").page(params[:page]).per(10)
+    @interested = @responses.where(status: "Interested").order("gigs.start_date").page(params[:page]).per(10)
   end
 end
