253f2e872685840818fa5ce74fe262c7b55214a7
Kate Howard <k.l.howard.1992@gmail.com>
2024-05-08 18:36:41 +0100

    Fix feed timezones (again)

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -8,7 +8,7 @@ class FeedController < ApplicationController
     @links = Link.order(created_at: :desc).to_a
     @reactions = Reaction.order(created_at: :desc).to_a
     @actions = (@gigs + @responses + @posts + @links + @reactions).sort! { |a, b| b.created_at <=> a.created_at }
-    @grouped_actions = @actions.group_by { |a| [a.created_at.to_datetime.strftime("%F %H:%M"), a.user_id] }.group_by { |(d, u), a| d.to_date }
+    @grouped_actions = @actions.group_by { |a| [a.created_at.to_datetime.strftime("%F %H:%M %Z"), a.user_id] }.group_by { |(d, u), a| d.to_date }
     @actions_by_date = Kaminari.paginate_array(@grouped_actions, total_count: @grouped_actions.count).page(params[:page]).per(3)
   end
 end
