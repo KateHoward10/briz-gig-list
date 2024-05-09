8894c880084b5390f3fe493399cf36d64cd64ac2
Kate Howard <k.l.howard.1992@gmail.com>
2024-05-09 17:06:22 +0100

    Allow user to delete own reactions

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,6 +1,7 @@
 class ReactionsController < ApplicationController
   before_action :authenticate
-  before_action :set_gig
+  before_action :set_gig, only: [:create, :update]
+  before_action :set_reaction, only: [:update, :destroy]
 
   def create
     @reaction = Reaction.new(reaction_params)
@@ -12,16 +13,25 @@ class ReactionsController < ApplicationController
   end
 
   def update
-    @reaction = Reaction.find(params[:id])
-
     if @reaction.update(reaction_params)
       redirect_to gig_path(@gig_id)
+    end
+  end
+
+  def destroy
+    if @reaction.destroy
+      redirect_to gig_path(@reaction.post.gig_id)
     end
   end
 
   private
     def set_gig
       @gig_id = Post.find(params[:reaction][:post_id]).gig_id
+    end
+
+    def set_reaction
+      @reaction = Reaction.find(params[:id])
+      return unless @reaction.user_id == current_user.id
     end
 
     def reaction_params
