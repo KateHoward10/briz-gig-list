ff9b1f870396038a0b3f049777ea20313a9aafd3
Kate Howard <k.l.howard.1992@gmail.com>
2024-05-10 17:09:18 +0100

    Update post menu opening

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,13 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from "stimulus-use"
 
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["button", "menu"]
+
+  connect() {
+    useClickOutside(this, { element: this.menuTarget, eventPrefix: false })
+  }
+
+  showMenuButton() {
+    this.buttonTarget.classList.remove("hidden")
+  }
+
+  hideMenuButton() {
+    this.buttonTarget.classList.add("hidden")
+  }
 
   openMenu() {
     this.menuTarget.classList.remove("hidden")
+    this.hideMenuButton()
   }
 
-  closeMenu() {
+  closeMenu(e) {
+    e.preventDefault()
     this.menuTarget.classList.add("hidden")
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,9 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@microlink/mql", to: "@microlink--mql.js" # @0.13.5
 pin "@joeattardi/emoji-button", to: "@joeattardi--emoji-button.js" # @4.6.4
+pin "stimulus-use" # @0.52.2
