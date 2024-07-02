9adf40f1bc7b9042f86711efacfae9451763fcd0
Kate Howard <k.l.howard.1992@gmail.com>
2024-07-02 13:18:10 +0100

    Click to fetch URL data

--- app/javascript/controllers/link_form_controller.js
+++ app/javascript/controllers/link_form_controller.js
@@ -1,55 +1,52 @@
 import { Controller } from '@hotwired/stimulus'
 import mql from '@microlink/mql'
 
-const separators = [
-  " Tickets | ",    // Dice
-  " | ",            // e.g. Beacon
-  " — ",            // e.g. Trinity
-  " at "            // Headfirst
-]
-
-const months = {
-  "jan": "01",
-  "feb": "02",
-  "mar": "03",
-  "apr": "04",
-  "may": "05",
-  "jun": "06",
-  "jul": "07",
-  "aug": "08",
-  "sep": "09",
-  "oct": "10",
-  "nov": "11",
-  "dec": "12"
-}
-
-const currentYear = new Date().getFullYear();
-
 export default class extends Controller {
-  static targets = ["url", "status", "text", "summary", "startDate", "endDate", "location", "venueList", "image", "preview", "removeButton"]
+  static targets = [
+    "urlField",
+    "fetchButton",
+    "status",
+    "textField",
+    "summary",
+    "startDate",
+    "endDate",
+    "location",
+    "venueList",
+    "image",
+    "preview",
+    "removeButton"
+  ]
 
   reset() {
+    this.textFieldTarget.value = "";
     this.statusTarget.textContent = "";
-    this.textTarget.disabled = false;
-    if (this.hasSummaryTarget) this.summaryTarget.disabled = false;
-    if (this.hasStartDateTarget) this.startDateTarget.disabled = false;
-    if (this.hasEndDateTarget) this.endDateTarget.disabled = false;
-    if (this.hasLocationTarget) this.locationTarget.disabled = false;
+    this.fetchButtonTarget.textContent = "Fetch data";
+    this.fetchButtonTarget.disabled = true;
+    this.textFieldTarget.disabled = false;
+    this.setGigFields(false);
   }
 
-  async handleURL() {
-    if (this.urlTarget.value === "") {
-      this.setFormData("");
+  activateFetchButton() {
+    this.fetchButtonTarget.disabled = !this.urlFieldTarget.value;
+  }
+
+  setGigFields(disabled) {
+    if (this.hasSummaryTarget) this.summaryTarget.disabled = disabled;
+    if (this.hasStartDateTarget) this.startDateTarget.disabled = disabled;
+    if (this.hasEndDateTarget) this.endDateTarget.disabled = disabled;
+    if (this.hasLocationTarget) this.locationTarget.disabled = disabled;
+  }
+
+  async fetchData() {
+    if (this.fetchButtonTarget.textContent === "STOP" || this.urlFieldTarget.value === "") {
       return this.reset();
     }
     this.statusTarget.textContent = "Checking URL...";
-    this.textTarget.disabled = true;
-    if (this.hasSummaryTarget) this.summaryTarget.disabled = true;
-    if (this.hasStartDateTarget) this.startDateTarget.disabled = true;
-    if (this.hasEndDateTarget) this.endDateTarget.disabled = true;
-    if (this.hasLocationTarget) this.locationTarget.disabled = true;
+    this.fetchButtonTarget.textContent = "STOP";
+    this.textFieldTarget.disabled = true;
+    this.setGigFields(true);
     try {
-      mql(this.urlTarget.value).then(({ status, data }) => {
+      mql(this.urlFieldTarget.value).then(({ status, data }) => {
         if (status == "success") {
           this.setFormData(data);
         } else {
@@ -62,7 +59,7 @@ export default class extends Controller {
   }
 
   setFormData({ publisher, title, url, image }) {
-    this.textTarget.value = publisher || null;
+    this.textFieldTarget.value = publisher || null;
     if (this.hasSummaryTarget && title) {
       let artist, location;
       separators.some((separator) => {
@@ -131,3 +128,27 @@ export default class extends Controller {
     }
   }
 }
+
+const separators = [
+  " Tickets | ",    // Dice
+  " | ",            // e.g. Beacon
+  " — ",            // e.g. Trinity
+  " at "            // Headfirst
+]
+
+const months = {
+  "jan": "01",
+  "feb": "02",
+  "mar": "03",
+  "apr": "04",
+  "may": "05",
+  "jun": "06",
+  "jul": "07",
+  "aug": "08",
+  "sep": "09",
+  "oct": "10",
+  "nov": "11",
+  "dec": "12"
+}
+
+const currentYear = new Date().getFullYear();
